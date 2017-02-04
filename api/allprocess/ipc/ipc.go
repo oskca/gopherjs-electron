c15c17d247b09792eebe3a41ca5523d14a8fb054
oskca <datago@yeah.net>
2017-02-04 11:15:11 +0800

    unify ipcMain and ipcRenderer in ipc.go

--- api/allprocess/ipc/ipc.go
+++ api/allprocess/ipc/ipc.go
@@ -1,77 +1,99 @@
 // Package ipc Communicate asynchronously from a renderer process to the main process.
-// Process: Renderer
+// Process: Renderer/Main
 //
-// The ipcRenderer module is an instance of the EventEmitter class.
+// The ipc module is an instance of the EventEmitter class.
 // It provides a few methods so you can send synchronous and asynchronous messages from
 // the render process (web page) to the main process. You can also receive replies from the main process.
 package ipc
 
 import (
+	"github.com/gopherjs/gopherjs/js"
 	electron "github.com/oskca/gopherjs-electron"
 	"github.com/oskca/gopherjs-electron/api"
+	"github.com/oskca/gopherjs-nodejs/eventemitter"
 )
 
-var ipcRenderer = electron.Get("ipcRenderer")
+type IPC struct {
+	*js.Object
+	*eventemitter.EventEmitter
+}
 
-// ipcRenderer
+func newIPC(obj *js.Object) *IPC {
+	ipc := new(IPC)
+	ipc.Object = obj
+	ipc.EventEmitter = eventemitter.New(obj)
+	return ipc
+}
+
+// Main returns ipcMain
+func Main() *IPC {
+	return newIPC(electron.Get("ipcMain"))
+}
+
+// Renderer returns ipcRenderer
+func Renderer() *IPC {
+	return newIPC(electron.Get("ipcRenderer"))
+}
+
+// ipc
 
 // See ipcMain for code examples.
 
 // Methods
 
-// The ipcRenderer module has the following method to listen for events and send messages:
+// The ipc module has the following method to listen for events and send messages:
 
-// ipcRenderer.on(channel, listener)
+// ipc.on(channel, listener)
 // channel String
 // listener Function
 // Listens to channel, when a new message arrives listener would be called with listener(event, args...).
-func On(channel string, listener api.IpcListener) {
-	ipcRenderer.Call("on", channel, listener)
+func (ipc *IPC) On(channel string, listener api.IpcListener) {
+	ipc.Call("on", channel, listener)
 }
 
-// ipcRenderer.once(channel, listener)
+// ipc.once(channel, listener)
 // channel String
 // listener Function
 // Adds a one time listener function for the event. This listener is invoked only the next time a message is sent to channel, after which it is removed.
-func Once(channel string, listener api.IpcListener) {
-	ipcRenderer.Call("once", channel, listener)
+func (ipc *IPC) Once(channel string, listener api.IpcListener) {
+	ipc.Call("once", channel, listener)
 }
 
-// ipcRenderer.removeListener(channel, listener)
+// ipc.removeListener(channel, listener)
 // channel String
 // listener Function
 // Removes the specified listener from the listener array for the specified channel.
-func RemoveListener(channel string, listener api.IpcListener) {
-	ipcRenderer.Call("removeListener", channel, listener)
+func (ipc *IPC) RemoveListener(channel string, listener api.IpcListener) {
+	ipc.Call("removeListener", channel, listener)
 }
 
-// ipcRenderer.removeAllListeners([channel])
+// ipc.removeAllListeners([channel])
 // channel String (optional)
 // Removes all listeners, or those of the specified channel.
-func RemoveAllListeners(channel ...string) {
+func (ipc *IPC) RemoveAllListeners(channel ...string) {
 	if len(channel) > 0 {
-		ipcRenderer.Call("removeAllListeners", channel[0])
+		ipc.Call("removeAllListeners", channel[0])
 	} else {
-		ipcRenderer.Call("removeAllListeners")
+		ipc.Call("removeAllListeners")
 	}
 }
 
-// ipcRenderer.send(channel[, arg1][, arg2][, ...])
+// ipc.send(channel[, arg1][, arg2][, ...])
 // channel String
 // ...args any[]
 // Send a message to the main process asynchronously via channel,
 // you can also send arbitrary arguments. Arguments will be serialized in JSON internally and
 // hence no functions or prototype chain will be included.
 // The main process handles it by listening for channel with ipcMain module.
-func Send(channel string, args ...interface{}) {
+func (ipc *IPC) Send(channel string, args ...interface{}) {
 	n := []interface{}{channel}
 	for _, arg := range args {
 		n = append(n, arg)
 	}
-	ipcRenderer.Call("send", n...)
+	ipc.Call("send", n...)
 }
 
-// ipcRenderer.sendSync(channel[, arg1][, arg2][, ...])
+// ipc.sendSync(channel[, arg1][, arg2][, ...])
 // channel String
 // ...args any[]
 // Send a message to the main process synchronously via channel, you can also send arbitrary arguments.
@@ -80,23 +102,23 @@ func Send(channel string, args ...interface{}) {
 // and replies by setting event.returnValue.
 // Note: Sending a synchronous message will block the whole renderer process, unless
 // you know what you are doing you should never use it.
-func SendSync(channel string, args ...interface{}) {
+func (ipc *IPC) SendSync(channel string, args ...interface{}) {
 	n := []interface{}{channel}
 	for _, arg := range args {
 		n = append(n, arg)
 	}
-	ipcRenderer.Call("sendSync", n...)
+	ipc.Call("sendSync", n...)
 }
 
-// ipcRenderer.sendToHost(channel[, arg1][, arg2][, ...])
+// ipc.sendToHost(channel[, arg1][, arg2][, ...])
 // channel String
 // ...args any[]
-// Like ipcRenderer.send but the event will be sent to the <webview> element in
+// Like ipc.send but the event will be sent to the <webview> element in
 // the host page instead of the main process.
-func SendToHost(channel string, args ...interface{}) {
+func (ipc *IPC) SendToHost(channel string, args ...interface{}) {
 	n := []interface{}{channel}
 	for _, arg := range args {
 		n = append(n, arg)
 	}
-	ipcRenderer.Call("sendToHost", n...)
+	ipc.Call("sendToHost", n...)
 }
